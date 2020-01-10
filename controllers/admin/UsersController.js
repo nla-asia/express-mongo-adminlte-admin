f23b6c0c265d58e45a4de018530b6166dce76290
Nay Lin Aung <naysumyat2011@gmail.com>
2020-01-10 17:55:00 +0630

    some updates

--- controllers/admin/UsersController.js
+++ controllers/admin/UsersController.js
@@ -1,4 +1,4 @@
-
+var User = require('../../models/user');
 /**
  * list users
  * @param 
@@ -7,8 +7,27 @@
  */
 
 exports.index = async (req, res, next) => {
-
     console.log(req.user);
+    try {
 
-    return res.render("admin/users/index", {user: req.user});
+        let data = await User.find();
+
+        return res.render("admin/users/index", {data: data});
+    } catch (error) {
+        return res.render("admin/users/index", {data: data});
+    }
 };
+
+/**
+ * list users datatable
+ * @param 
+ * @public
+ * @post
+ */
+
+exports.index_json = async (req, res, next) => {
+    console.log(req.user);
+    return res.json({success:true});
+};
+
+
